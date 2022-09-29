44a4ec3ef70af902430a11fb34199698ec978903
Alexis Ortiz <olmedo.aortiz@gmail.com>
2022-09-28 21:11:43 -0400

    [MOD] Primera vista el login.

--- src/main/java/com/proyecto/is2/proyecto/security/SecurityConfiguration.java
+++ src/main/java/com/proyecto/is2/proyecto/security/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/login",
+                .antMatchers("/login",
                         "/css/**","/js/**","/img/**")
                 .permitAll()
                 .anyRequest()
