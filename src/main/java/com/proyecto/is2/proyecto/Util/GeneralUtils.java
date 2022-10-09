d1435033f1c750ccd3b9f1180d199b617286e458
Alexis Ortiz <aortiz@roshka.com>
2022-10-09 13:28:25 -0300

    [ADD] Static files (Bootstrap, jquert, etc).

---
+++ src/main/java/com/proyecto/is2/proyecto/Util/GeneralUtils.java
@@ -0,0 +1,8 @@
+package com.proyecto.is2.proyecto.Util;
+
+public class GeneralUtils {
+    // Admin Default config
+    public final static String ADMIN_USER = "admin";
+    public final static String ADMIN_EMAIL = "admin@gmail.com";
+    public final static String ADMIN_PASS = "12345678";
+}
